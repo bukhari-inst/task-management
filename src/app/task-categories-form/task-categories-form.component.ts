84558c3878cb6d923d2d2c90c32497c64997f53d
Bukhari_Inst <bukhari414647@gmail.com>
2023-02-06 09:41:06 +0700

    create component task categories, task

---
+++ src/app/task-categories-form/task-categories-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-categories-form',
+  templateUrl: './task-categories-form.component.html',
+  styleUrls: ['./task-categories-form.component.scss']
+})
+export class TaskCategoriesFormComponent {
+
+}
