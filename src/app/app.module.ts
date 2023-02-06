dbeb89cda2b5b9b09487eb8f125c3f28997d24fd
Bukhari_Inst <bukhari414647@gmail.com>
2023-02-06 09:41:11 +0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { TaskCategoriesFormComponent } from './task-categories-form/task-categories-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskFormComponent,
+    TaskCategoriesFormComponent
   ],
   imports: [
     BrowserModule
