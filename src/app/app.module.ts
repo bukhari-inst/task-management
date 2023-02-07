e75aeac5d4dd1faf2c0426ffb75b3568b7ad71c5
Bukhari_Inst <bukhari414647@gmail.com>
2023-02-07 16:29:46 +0700

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { MessageComponent } from './message/message.component';
 import { TaskFormComponent } from './task-form/task-form.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, MessageComponent, TaskFormComponent, TaskListComponent],
+  declarations: [AppComponent, MessageComponent, TaskFormComponent, TaskListComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
